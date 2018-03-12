83ff3eab4e5af27bd6414f17baae6be70c3fa538
Jeferson Cruz <jeferson.hck@gmail.com>
2018-03-12 20:40:12 -0300

    Print MDFe

--- src/main/java/br/indie/fiscal4j/danfe/MDFeDanfeReport.java
+++ src/main/java/br/indie/fiscal4j/danfe/MDFeDanfeReport.java
@@ -3,10 +3,16 @@ package br.indie.fiscal4j.danfe;
 import br.indie.fiscal4j.mdfe3.classes.nota.MDFeProcessada;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRXmlDataSource;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,7 +34,7 @@ public class MDFeDanfeReport {
         return JasperExportManager.exportReportToPdf(print);
     }
 
-    private JasperPrint createJasperPrintMDFe(byte[] logoEmpresa, String rodape) throws IOException, JRException {
+    private JasperPrint createJasperPrintMDFe(byte[] logoEmpresa, String rodape) throws IOException, JRException, ParserConfigurationException, SAXException {
         try (InputStream in = MDFeDanfeReport.class.getClassLoader().getResourceAsStream("danfe/mdfe/DANFE_MDFE_RODO_RETRATO.jasper")) {
             final JRPropertiesUtil jrPropertiesUtil = JRPropertiesUtil.getInstance(DefaultJasperReportsContext.getInstance());
             jrPropertiesUtil.setProperty("net.sf.jasperreports.xpath.executer.factory", "net.sf.jasperreports.engine.util.xml.JaxenXPathExecuterFactory");
@@ -36,7 +42,15 @@ public class MDFeDanfeReport {
             parameters.put("LOGO_EMPRESA", (logoEmpresa == null ? null : new ByteArrayInputStream(logoEmpresa)));
             parameters.put("RODAPE", rodape);
 
-            return JasperFillManager.fillReport(in, parameters, new JRXmlDataSource(this.mdFeProcessada.toString(), "/mdfeProc"));
+            return JasperFillManager.fillReport(in, parameters, new JRXmlDataSource(convertStringXMl2DOM(mdFeProcessada.toString()), "/mdfeProc"));
+        }
+    }
+
+    private Document convertStringXMl2DOM(String xml) throws ParserConfigurationException, IOException, SAXException {
+        try (StringReader stringReader = new StringReader(xml)) {
+            InputSource inputSource = new InputSource();
+            inputSource.setCharacterStream(stringReader);
+            return DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(inputSource);
         }
     }
 
